8bbbd04d86c0e35519ce92cedcd1d9823e655017
Eric Ripa <eric.ripa@me.com>
2013-02-23 17:14:40 +0100

    added some cli options and user options to select legacy or dry-run mode. Will also download directly to a String instead of saving to disk first.

--- malhosts.groovy
+++ malhosts.groovy
@@ -8,19 +8,54 @@
 */
 
 import Host
-import ProgramExitException
+import java.text.*
+
+def __version__ = "0.1"
+
+def parseArgs(args) {
+  def cli = new CliBuilder(usage: 'malhosts.groovy --help/-h --legacy/-l --dry-run/-d')
+
+  cli.with {
+      h longOpt: 'help', 'Show usage information'
+      l longOpt: 'legacy', 'Use legacy format (127.0.0.1 instead of faster 0.0.0.0)'
+      d longOpt: 'dry-run', 'Don\'t do anything, only display what will happen'
+  }
+
+  def options = cli.parse(args)
+  if (options.h) {
+    cli.usage()
+    System.exit(0)
+  }
+  return options
+}
+
+
+// Method for selecting the desired hosts file
+def getUrl(legacy) {
+  switch(legacy) {
+    case true:
+      // use legacy formatted file for older systems 127.0.0.1
+      def url = "http://someonewhocares.org/hosts/hosts"
+      break
+    case false:
+      // use faster syntax with 0.0.0.0
+      def url = "http://someonewhocares.org/hosts/zero/hosts"
+      break
+  }
+}
+
+// Method for downloading a file to a string
+def getStringFromUrl(url) {
+  try {
+    String hosts = url.toURL().getText()
+  } catch (java.io.FileNotFoundException e) {
+    println "File ${e.message} not found!"
+    throw new RuntimeException()
+  }
+}
 
 // to implement proxy support later on, look at this:
 //System.properties.putAll( ["http.proxyHost":"proxy-host", "http.proxyPort":"proxy-port","http.proxyUserName":"user-name", "http.proxyPassword":"proxy-passwd"] )
-
-
-def download(address)
-{
-    def file = new FileOutputStream(address.tokenize("/")[-1])
-    def out = new BufferedOutputStream(file)
-    out << new URL(address).openStream()
-    out.close()
-}
 
 def host = new Host(['ip': '0.0.0.0', 'hostname': 'lanboll.com', 'global': true])
 
@@ -28,16 +63,19 @@ assert host.ip == '0.0.0.0'
 assert host.hostname == 'lanboll.com'
 assert host.global
 
-// 0 file
-// http://someonewhocares.org/hosts/zero/hosts
+def options = parseArgs(args)
+println "malhosts ${__version__}\nlegacy: ${options.l}, dry-run: ${options.d}"
 
-// 127 file
-// http://someonewhocares.org/hosts/hosts
+def url = getUrl(options.l)
+
+List malhosts_BEGIN = ["############ MALHOSTS BEGIN MARK, DO NOT REMOVE ############"]
+List malhosts_END = ["############ MALHOSTS END MARK, DO NOT REMOVE ############"]
+List hosts_list = getStringFromUrl(url).split('\n')
+
+host_list_with_marks = malhosts_BEGIN += hosts_list
+host_list_with_marks = host_list_with_marks += malhosts_END
 
 
-try {
-  def hosts_file = download("http://someonewhocares.org/hosts/zero/hosts1")
-} catch (java.io.FileNotFoundException e) {
-   println "File ${e.message} not found!"
-   throw new ProgramExitException(1)
-}
+println host_list_with_marks[127]
+
+
